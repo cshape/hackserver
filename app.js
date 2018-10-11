d540c1b6033b822bd70be1dedba63ac9b6a4f59c
Cale Shapera <caleshapera@gmail.com>
2018-10-11 10:36:33 -0700

    signin and signup flows and user endpoints

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 
 const parseurl = require('parseurl');
 const bodyParser = require('body-parser');
@@ -48,6 +49,144 @@ app.get('/', function(req, res) {
   res.json('index');
 
 });
+
+//list users
+
+app.get('/api/users', function(req, res) {
+  Users.find({}).then(eachOne => {
+    res.json(eachOne);
+    })
+  });
+
+//sign up user
+
+app.post('/api/users/signup', (req, res, next) => {
+    const { body } = req;
+    const { password } = body;
+    let { email } = body;
+    
+    if (!email) {
+      return res.send({
+        success: false,
+        message: 'Error: Email cannot be blank.'
+      });
+    }
+    if (!password) {
+      return res.send({
+        success: false,
+        message: 'Error: Password cannot be blank.'
+      });
+    }
+    email = email.toLowerCase();
+    email = email.trim();
+    // Steps:
+    // 1. Verify email doesn't exist
+    // 2. Save
+    Users.find({
+      email: email
+    }, (err, previousUsers) => {
+      if (err) {
+        return res.send({
+          success: false,
+          message: 'Error: sucks to be you, man.'
+        });
+      } else if (previousUsers.length > 0) {
+        return res.send({
+          success: false,
+          message: 'Error: Account already exist.'
+        });
+      }
+      // Save the new user
+      const newUser = new Users();
+      newUser.email = email;
+      newUser.password = newUser.generateHash(password);
+      newUser.save((err, user) => {
+        if (err) {
+          return res.send({
+            success: false,
+            message: 'Error: Server error u bad man'
+          });
+        }
+        return res.send({
+          success: true,
+          message: 'Signed up'
+        });
+      });
+    });
+  });
+
+// sign in
+
+app.post('/api/users/signin', (req, res, next) => {
+    const { body } = req;
+    const {
+      password
+    } = body;
+    let {
+      email
+    } = body;
+    if (!email) {
+      return res.send({
+        success: false,
+        message: 'Error: Email cannot be blank.'
+      });
+    }
+    if (!password) {
+      return res.send({
+        success: false,
+        message: 'Error: Password cannot be blank.'
+      });
+    }
+    email = email.toLowerCase();
+    email = email.trim();
+    Users.find({
+      email: email
+    }, (err, users) => {
+      if (err) {
+        console.log('err 2:', err);
+        return res.send({
+          success: false,
+          message: 'Error: server error'
+        });
+      }
+      if (users.length != 1) {
+        return res.send({
+          success: false,
+          message: 'Error: Invalid'
+        });
+      }
+      const user = users[0];
+      if (!user.validPassword(password)) {
+        return res.send({
+          success: false,
+          message: 'Error: Invalid'
+        });
+      }
+      // Otherwise correct user
+      return res.send({
+        success: true,
+        message: 'Success! Logged in!'
+      })
+    });
+  });
+
+
+
+
+// app.post('/api/users', function(req, res, next) {
+//   Users.create({
+//     email: req.body.username,
+//     password: req.body.password,
+//     firstname: req.body.firstname,
+//     lastname: req.body.lastname
+//   }).then(user => {
+//     res.json(user)
+//   });
+// });
+
+//log in user
+
+
 
 //ideas
 
--- models/users.js
+++ models/users.js
@@ -1,13 +1,25 @@
 var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
 
 
 // Define schema
 var Schema = mongoose.Schema;
 
 var UserSchema = new Schema({
-    username: String,
-    password: String
+    email: String,
+    password: String,
+    firstname: String,
+    lastname: String,
+    leader: String,
+    member: String
 });
+
+UserSchema.methods.generateHash = function(password) {
+  return bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+};
+UserSchema.methods.validPassword = function(password) {
+  return bcrypt.compareSync(password, this.password);
+};
 
 //Export function to create "SomeModel" model class
 module.exports = mongoose.model('UserModel', UserSchema );
